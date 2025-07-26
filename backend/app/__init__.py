542c727778f99f6f21b5928047635436f6ce2aa7
Gael <gaelgilbello27@gmail.com>
2025-07-26 11:10:08 -0700

    updated to remove db stuff for now

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,22 +1,24 @@
 from flask import Flask
 from app.config import Config
-from app.extensions import db, migrate, socketio
+from app.extensions import migrate, socketio
 from flask_cors import CORS
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    
+
     # Initialize extensions
-    db.init_app(app)
-    migrate.init_app(app, db)
+    # db.init_app(app)
+    # migrate.init_app(app, db)
     socketio.init_app(app)
-    
+
     # Enable CORS
     CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 
     # Import and register blueprints
-    from app.user.controllers import users as users_blueprint
-    app.register_blueprint(users_blueprint, url_prefix='/api/users')
+    # from app.user.controllers import users as users_blueprint
+
+    # app.register_blueprint(users_blueprint, url_prefix="/api/users")
 
     return app
--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_socketio import SocketIO  
+from flask_socketio import SocketIO
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 migrate = Migrate()
 socketio = SocketIO(cors_allowed_origins="http://localhost:5173")
