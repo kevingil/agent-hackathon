1e7dc935b64aa92d85c70b4727bbdb14f8bc7344
Gael <gaelgilbello27@gmail.com>
2025-07-26 11:59:31 -0700

    added templates for tools, yet to be implemented

--- backend/app/agents/MCP/server.py
+++ backend/app/agents/MCP/server.py
@@ -1,12 +1,11 @@
-import json
-import requests
 from mcp.server.fastmcp import FastMCP
+from pathlib import Path
 
-import wikipedia
-import datetime
-import xml.etree.ElementTree as ET
+from .db_connection import StoreService
 
-ARXIV_NAMESPACE = "{http://www.w3.org/2005/Atom}"
+SQLLITE_DB = Path(__file__).parent.parent.parent.parent.parent.parent / "store.db"
+
+store_service = StoreService()
 
 # Create an MCP server
 mcp = FastMCP(
@@ -16,86 +15,82 @@ mcp = FastMCP(
 )
 
 
-# a function to search inside a json file
 @mcp.tool(
-    name="search_kb",
-    description="Get the answer to the users question from the knowledge base",
+    name="search",
+    description="Search inside a json file",
 )
-def search_kb() -> dict:
-    """
-    Load the whole knowledge base from the JSON file.
-    (This is a mock function for demonstration purposes, we don't search)
-    """
-    with open("../resources/kb.json", "r") as f:
-        return json.load(f)
+def write_db(query: str) -> str:
+    pass
 
 
-# get weather tool
 @mcp.tool(
-    name="get_weather",
-    description="Get current temperature for provided coordinates in celsius",
+    name="add_to_cart",
+    description="Add a part to the cart given the part id",
 )
-def get_weather(latitude, longitude):
-    response = requests.get(
-        f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
-    )
-    data = response.json()
-    return data["current"]
-
-
-# wiki search tool
-@mcp.tool(
-    name="wiki_search",
-    description="Get current temperature for provided coordinates in celsius",
-)
-def wiki_search(query: str, sentences: int = 2) -> str:
-    """
-    Searches Wikipedia for the given query and returns a summary.
-
+def add_to_cart(part_id: str | int) -> str:
+    """Add a part to the cart
     Args:
-        query (str): The search term.
-        sentences (int): Number of summary sentences to return.
+        part_id (str | int): The part id to add to the cart
 
     Returns:
-        str: Summary of the top Wikipedia page match.
+        str: A message indicating the part was added to the cart
     """
-    try:
-        summary = wikipedia.summary(query, sentences=sentences)
-        return summary
-    except wikipedia.exceptions.DisambiguationError as e:
-        return f"DisambiguationError: The query '{query}' may refer to multiple things:\n{e.options[:5]}"
-    except wikipedia.exceptions.PageError:
-        return f"No Wikipedia page found for '{query}'."
-    except Exception as e:
-        return f"An error occurred: {e}"
+    message = ""
+
+    return message
 
 
 @mcp.tool(
-    name="save_txt",
-    description="Save text to a .txt file",
+    name="remove_from_cart",
+    description="Remove a part to the cart given the part id",
 )
-def save_txt(text: str, filename: str = "output.txt") -> str:
-    """Saves the provided text to a .txt file."""
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    formatted_text = f"--- Research Output ---\nTimestamp: {timestamp}\n\n{text}\n\n"
+def remove_from_cart(part_id: str | int) -> str:
+    """Remove a part from the cart
+    Args:
+        part_id (str | int): The part id to remove from the cart
 
-    with open(filename, "a", encoding="utf-8") as f:
-        f.write(formatted_text)
+    Returns:
+        str: A message indicating the part was removed from the cart
+    """
+    message = ""
 
-    return f"Data successfully saved to {filename}"
+    return message
 
 
-@mcp.tool(name="arxiv_search", description="Search arxiv")
-def arxiv_search(query: str) -> str:
-    """Searches arxiv"""
-    url = f"http://export.arxiv.org/api/query?search_query=all:{query}&start=0&max_results=1"
-    res = requests.get(url)
-    et_root = ET.fromstring(res.content)
-    for entry in et_root.findall(f"{ARXIV_NAMESPACE}entry"):
-        title = entry.find(f"{ARXIV_NAMESPACE}title").text.strip()
-        summary = entry.find(f"{ARXIV_NAMESPACE}summary").text.strip()
-    return json.dumps({"title": title, "summary": summary})
+@mcp.tool(name="checkout_cart", description="Checkout the cart")
+def checkout_cart(cart_id: str | int) -> str:
+    """
+    Checkout the cart
+    Args:
+        cart_id (str | int): The cart id to checkout
 
+    Returns:
+        str: A message indicating the cart was checked out
+    """
+    message = ""
+
+    return message
+
+
+@mcp.tool(name="find_inventory", description="Search the database inventory for a part")
+def find_inventory(keyword: str) -> str:
+    """
+    Search the database inventory for a part using a keryword
+    Args:
+        keyword: The keyword to search for in the inventory database
+
+    Returns:
+        str: A message indicating the part was found in the inventory
+    """
+    message = ""
+
+    return message
+
+
+# TODO: add tools to read from dummy database (store)
+# TODO: add tools to update database after exectuion
+# TODO: add to user car cart (partNum), remove from cart, checkout cart
+# TODO: find inventory queryword serch or rag search (parnum qwuery) and also natural language query
 
 # Run the server
 if __name__ == "__main__":
