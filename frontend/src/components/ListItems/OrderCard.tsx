a97d96a88a05c434c2a9967d533a8fdfa24527d2
Gael <gaelgilbello27@gmail.com>
2025-07-26 14:49:09 -0700

    updated bold list items

--- frontend/src/components/ListItems/OrderCard.tsx
+++ frontend/src/components/ListItems/OrderCard.tsx
@@ -10,12 +10,25 @@ const OrderCard: React.FC<OrderProps> = ({ order }) => {
           <Card.Title>{order.name}</Card.Title>
         </Card.Body>
         <ListGroup className="list-group-flush">
-          <ListGroup.Item>{order.description}</ListGroup.Item>
-          <ListGroup.Item>{order.cost}</ListGroup.Item>
-          <ListGroup.Item>{order.list_price}</ListGroup.Item>
-          <ListGroup.Item>{order.quantity}</ListGroup.Item>
-          <ListGroup.Item>{order.created_at}</ListGroup.Item>
-          <ListGroup.Item>{order.updated_at}</ListGroup.Item>
+          <ListGroup.Item>
+            <strong>Description:</strong> {order.description}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <strong>Corst:</strong> {order.cost}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <strong>List Price:</strong>
+            {order.list_price}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <strong>Quantity:</strong> {order.quantity}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <strong>Created At:</strong> {order.created_at}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <strong>Updated At:</strong> {order.updated_at}
+          </ListGroup.Item>
         </ListGroup>
       </Card>
     </>
